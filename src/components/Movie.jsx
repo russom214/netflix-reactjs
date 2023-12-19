ee56be01de467297e30b27907d082c825d73b5ad
russom214 <russom214@gmail.com>
2023-12-19 13:55:01 -0500

    Fininshed setting up the home/landing page

---
+++ src/components/Movie.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Movie = ({ item }) => {
+  return (
+    <div className="w-[160px] sm:w-[200px] lg:w-[200px] inline-block cursor-pointer relative">
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+        alt={item.title}
+      />
+      <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+        <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+          {item?.title}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
