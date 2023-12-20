a2f067bc6ffdc0b7915913288e6ab73a17bd9378
russom214 <russom214@gmail.com>
2023-12-20 17:20:01 -0500

    Finalized the project/ Saved show functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -15,7 +15,7 @@ export function AuthContextProvider({ children }) {
 
   function signUp(email, password) {
     createUserWithEmailAndPassword(auth, email, password);
-    setDoc(doc(db, user, email), {
+    setDoc(doc(db, 'users', email), {
         savedShows: []
     })
   }
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-// import SavedShows from "../components/SavedShows";
+import SavedShows from "../components/SavedShows";
+
+import backgroundimg from "../assests/backgroundimg.jpg"
 
 const Account = () => {
   return (
@@ -7,7 +9,7 @@ const Account = () => {
       <div className="w-full text-white">
         <img
           className="w-full h-[400px] object-cover"
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          src={backgroundimg}
           alt="/"
         />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
@@ -15,7 +17,7 @@ const Account = () => {
           <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
         </div>
       </div>
-      {/* <SavedShows /> */}
+      <SavedShows />
     </>
   );
 };
