a4a1a437dad41851829c2b27c37cd89ae53dad3d
russom214 <russom214@gmail.com>
2023-12-20 16:53:00 -0500

    Firebase Authentication, Login/Logout pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext.js';
 
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import Account from './pages/Account.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/account' element={
+          <ProtectedRoute>
+            <Account/>
+          </ProtectedRoute>}/>
       </Routes>
-      
+    </AuthContextProvider>
     </>
   );
 }
